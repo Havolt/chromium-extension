ef70c12577e5d20b7fcba41b565aea8264dcb64f
Mark <markfitz815@gmail.com>
2024-03-25 20:37:08 +0000

    Save a note with storage API

--- src/scripts/base.js
+++ src/scripts/base.js
@@ -2,12 +2,23 @@ console.log("Script initialized");
 
 // get the button by its id and store it for other functions
 const addButton = document.getElementById("add-note");
+const newNote = document.getElementById("new-note");
 
 const addNote = () => {
-  console.log("Note added");
+    const note = newNote.value;
+    console.log({note});
+    if(note.length > 0){
+        chrome.storage.sync.set({note}, () => {
+            console.log("Note saved");
+        });
+        newNote.value = "";
+    }
 };
 
 
 (() => { 
     addButton.addEventListener("click", addNote);
+    chrome.storage.sync.get("note", (data) => { 
+        console.log("Data received", data);
+    })
 })();
