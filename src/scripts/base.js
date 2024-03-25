46e254111eede34036f78d3652b82861f0dbb605
Mark <markfitz815@gmail.com>
2024-03-25 20:16:06 +0000

    Add a function call to the button

--- src/scripts/base.js
+++ src/scripts/base.js
@@ -1,1 +1,13 @@
 console.log("Script initialized");
+
+// get the button by its id and store it for other functions
+const addButton = document.getElementById("add-note");
+
+const addNote = () => {
+  console.log("Note added");
+};
+
+
+(() => { 
+    addButton.addEventListener("click", addNote);
+})();
