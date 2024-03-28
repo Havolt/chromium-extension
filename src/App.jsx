a8fc14eaed0887cb55c419c16e8c4429ad89dc67
Mark <markfitz815@gmail.com>
2024-03-28 20:14:01 +0000

    Get user notes and store them in state

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,31 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import './App.css'
 
 function App() {
   const EXTENTIION_TITLE = 'Qutes'
+  
+  // State
+  const [notes, setNotes] = useState([])
+
+  // Refs
+  const inputRef = useRef(null)
+
+  const handleNoteSubmit = (e) => { 
+    e.preventDefault()
+    console.log(inputRef.current.value)
+    if (inputRef.current.value) {
+      setNotes([...notes, inputRef.current.value])
+      inputRef.current.value = ''
+    }
+  }
 
   return (
     <>
       <h1>{EXTENTIION_TITLE}</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      <form onSubmit={handleNoteSubmit} className="note-form">
+        <input ref={inputRef} type="text" />
+        <button>+</button>
+      </form>
     </>
   )
 }
