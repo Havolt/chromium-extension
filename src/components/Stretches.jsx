7b5b68ba36b6567b96d504b6c096ce3cd1cdae6b
Mark <markfitz815@gmail.com>
2024-03-31 19:50:34 +0100

    Add styling to list of stretches

--- src/components/Stretches.jsx
+++ src/components/Stretches.jsx
@@ -1,16 +1,25 @@
 import {useState} from 'react'
 import { stretches } from '../json/stretches'
+import './Stretches.scss'
 
 function Stretches() {
   const [stretchesData, setStretchesData] = useState(stretches.map(stretch => ({ ...stretch, completed: false })))
+
+  const handleCheckboxChange = (index) => {
+    const newStretchesData = [...stretchesData]
+    newStretchesData[index].completed = !newStretchesData[index].completed
+    setStretchesData(newStretchesData)
+  }
   return (
-    <div>
-      <ul>
-        { stretchesData.map((stretch, index) => (
-          <li key={index}>{ stretch.name}</li>
-        ))}
-      </ul>
-    </div>
+    <ul className="stretches">
+      { stretchesData.map((stretch, index) => (
+        <li key={index}>
+          <label>{ stretch.name}</label>
+          <input type="checkbox" checked={stretch.completed} onChange={() => {handleCheckboxChange(index)} } />
+          <button>{'>'}</button>
+        </li>
+      ))}
+    </ul>
   )
 }
 
